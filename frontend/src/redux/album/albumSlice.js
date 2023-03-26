3e542703f675268e20386ba79a741327257760cf
artiq75 <artiq75@icloud.com>
2023-03-26 16:16:27 +0200

    Fix search

--- frontend/src/redux/album/albumSlice.js
+++ frontend/src/redux/album/albumSlice.js
@@ -1,71 +1,73 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import { api } from "../../constants/apiConstant"
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { api } from '../../constants/apiConstant'
 
 const slice = createSlice({
-    //on lui donne un nom
-    name: 'albums',
-    //on initialise les valeurs par défault
-    initialState: {
-        loading: false,
-        albums: [],
-        searchTitle: [],
-        searchBiography: [],
+  //on lui donne un nom
+  name: 'albums',
+  //on initialise les valeurs par défault
+  initialState: {
+    loading: false,
+    albums: [],
+    searchTitle: [],
+    searchBiography: []
+  },
+  //reducer permet de remplir les valeurs des states
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload
     },
-    //reducer permet de remplir les valeurs des states
-    reducers: {
-        setLoading: (state, action) => {
-            state.loading = action.payload;
-        },
-        setAlbums: (state, action) => {
-            state.albums = action.payload;
-        },
-        setSearchTitle: (state, action) => {
-            state.searchTitle = action.payload;
-        },
-        setSearchBiography: (state, action) => {
-            state.searchBiography = action.payload;
-        },
+    setAlbums: (state, action) => {
+      state.albums = action.payload
+    },
+    setSearchTitle: (state, action) => {
+      state.searchTitle = action.payload
+    },
+    setSearchBiography: (state, action) => {
+      state.searchBiography = action.payload
     }
-
+  }
 })
 
 //on exporte sous forme de constantes les "setter" à l'application
-export const { setLoading, setAlbums, setSearchTitle, setSearchBiography } = slice.actions;
+export const { setLoading, setAlbums, setSearchTitle, setSearchBiography } =
+  slice.actions
 //on crée la méthode qui permet de récupérer les infos en BDD
-export const fetchAlbums = () => async dispatch => {
-    try {
-        //on passe setLoading à true pour gérer le chargement des données
-        dispatch(setLoading(true));
-        //on va chercher les infos en BDD
-        const response = await axios.get(`${api}/albums?isActive=true`);
-        //on set les données reçu dans notre slice grace à setAlbums
-        dispatch(setAlbums(response.data));
-        dispatch(setLoading(false));
-
-    } catch (error) {
-        console.error(error);
-        dispatch(setLoading(false));
-    }
+export const fetchAlbums = () => async (dispatch) => {
+  try {
+    //on passe setLoading à true pour gérer le chargement des données
+    dispatch(setLoading(true))
+    //on va chercher les infos en BDD
+    const response = await axios.get(`${api}/albums?isActive=true`)
+    //on set les données reçu dans notre slice grace à setAlbums
+    dispatch(setAlbums(response.data))
+    dispatch(setLoading(false))
+  } catch (error) {
+    console.error(error)
+    dispatch(setLoading(false))
+  }
 }
 
-export const fetchSearch = (word) => async dispatch => {
-    try {
-        //on passe setLoading à true pour gérer le chargement des données
-        dispatch(setLoading(true));
-        //on va chercher les infos en BDD
-        const responseTitle = await axios.get(`${api}/albums?isActive=true&title=${word}`);
-        const responseBiography = await axios.get(`${api}/albums?isActive=true&artist.biography=${word}`);
-        //on set les données reçu dans notre slice grace à setAlbums
-        dispatch(setSearchTitle(responseTitle.data));
-        dispatch(setSearchBiography(responseBiography.data));
-        dispatch(setLoading(false));
-
-    } catch (error) {
-        console.error(error);
-        dispatch(setLoading(false));
-    }
+export const fetchSearch = (word) => async (dispatch) => {
+  try {
+    //on passe setLoading à true pour gérer le chargement des données
+    dispatch(setLoading(true))
+    //on va chercher les infos en BDD
+    const responseTitle = await axios.get(
+      `${api}/albums?isActive=true&title=${word}`
+    )
+    const responseBiography = await axios.get(
+      `${api}/albums?isActive=true&artist.biography=${word}`
+    )
+    //on set les données reçu dans notre slice grace à setAlbums
+    dispatch(setSearchTitle(responseTitle.data))
+    dispatch(setSearchBiography(responseBiography.data))
+    dispatch(setLoading(false))
+  } catch (error) {
+    console.error(error)
+    dispatch(setLoading(false))
+  }
 }
 
 //on exporte notre reducer
-export default slice.reducer;
+export default slice.reducer
--- frontend/src/screens/Search.jsx
+++ frontend/src/screens/Search.jsx
@@ -1,14 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Searchbar from '../components/Searchbar'
+import Loader from '../components/Loader'
 import { selectAlbumsData } from '../redux/album/albumSelector'
+import AlbumCard from '../components/AlbumCard'
 
 const Search = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
 
-  const { searchTitle,searchBiography, loading } = useSelector(selectAlbumsData) 
-  
-  return (
-    <Searchbar/>
+  const { loading, searchTitle, searchBiography } =
+    useSelector(selectAlbumsData)
+
+  const albums = useMemo(() => {
+    if (searchTitle) return searchTitle['hydra:member']
+    return searchBiography['hydra:member']
+  }, [searchTitle, searchBiography])
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Searchbar />
+      <div className="flex flex-col">
+        <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+          Tous les albums
+        </h2>
+        <div className="flex flex-wrap md:justify-start justify-center gap-8">
+          {/* on va mapper sur dataAlbum dès que les infos sont chargées */}
+          {albums &&
+            albums.map((data, index) => {
+              return (
+                //on appelle le composant AlbumCard
+                <AlbumCard
+                  //on passe key en paramètre pour que chaque card est une clé unique
+                  key={index}
+                  //on passe data pour envoyer les infos de chaque album au composant
+                  data={data}
+                  //on passe le tableau de chanson
+                  songs={data.songs}
+                  //si le player le player est actif (valeur store)
+                  isPlaying={isPlaying}
+                  //pour activeSong on passe la valeur du store
+                  activeSong={activeSong}
+                  //on donne 0 pour lire la première piste de l'album
+                  index={0}
+                />
+              )
+            })}
+        </div>
+      </div>
+    </>
   )
 }
 
